3ab169c7960fbfa418b5e3b9f7d94b665218a1ab
Aaron Frase <afrase91@gmail.com>
2024-03-19 11:30:56 -0400

    second stage

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -13,9 +14,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	stream := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("Failed to close connection")
+		}
+	}(conn)
+
+	_, err := stream.WriteString("HTTP/1.1 200 OK\r\n\r\n")
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Failed to write to socket")
+		os.Exit(1)
+	}
+
+	err = stream.Flush()
+	if err != nil {
+		fmt.Println("Failed to flush to socket")
 		os.Exit(1)
 	}
 }
