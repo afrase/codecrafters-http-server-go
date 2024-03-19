72a95272ee8bf35d18cbbcc5bf43949510ae3911
Aaron Frase <afrase91@gmail.com>
2024-03-19 11:52:42 -0400

    third stage

--- app/server.go
+++ app/server.go
@@ -5,10 +5,31 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
+var statusCodeToString = map[int]string{
+	200: "OK",
+	404: "Not Found",
+}
+
+type Request struct {
+	Method string
+	Path   string
+}
+
+type Response struct {
+	StatusCode int
+	Body       string
+}
+
+func (r Response) String() string {
+	statusText := statusCodeToString[r.StatusCode]
+	return fmt.Sprintf("HTTP/1.1 %d %s\r\n\r\n%s", r.StatusCode, statusText, r.Body)
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", ":4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -30,13 +51,26 @@ func handleConnection(conn net.Conn) {
 	defer func(conn net.Conn) {
 		err := conn.Close()
 		if err != nil {
-			fmt.Println("Failed to close connection")
+			fmt.Println("Failed to close connection: ", err.Error())
 		}
 	}(conn)
 
-	_, err := stream.WriteString("HTTP/1.1 200 OK\r\n\r\n")
+	request, err := parseRequest(stream.Reader)
 	if err != nil {
-		fmt.Println("Failed to write to socket")
+		fmt.Println("Failed to parse request: ", err.Error())
+		os.Exit(1)
+	}
+
+	var response Response
+	if request.Path == "/" {
+		response = Response{StatusCode: 200}
+	} else {
+		response = Response{StatusCode: 404}
+	}
+
+	_, err = stream.WriteString(response.String())
+	if err != nil {
+		fmt.Println("Failed to write to socket: ", err.Error())
 		os.Exit(1)
 	}
 
@@ -45,4 +79,14 @@ func handleConnection(conn net.Conn) {
 		fmt.Println("Failed to flush to socket")
 		os.Exit(1)
 	}
+}
+
+func parseRequest(reader *bufio.Reader) (Request, error) {
+	first, err := reader.ReadString('\n')
+	if err != nil {
+		return Request{}, err
+	}
+
+	parts := strings.Split(first, " ")
+	return Request{Method: parts[0], Path: parts[1]}, nil
 }
